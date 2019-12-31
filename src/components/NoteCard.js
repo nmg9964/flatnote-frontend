24302ebae37cdb9faa27fd34ef74e5ef5c26971a
NaveedG <nganjani@protonmail.com>
2019-12-31 14:23:15 -0600

    modified notecard onClick

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -3,12 +3,32 @@ import { connect } from 'react-redux'
 import { deleteNote } from '../actions/notes'
 
 class NoteCard extends Component {
+
+  handleOnClick = (note) => {
+    const reqObj = 
+    {
+      method: 'DELETE',
+      headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+      }
+    }
+
+    fetch(`http://localhost:3001/notes/${note.id}`, reqObj)
+    .then(resp => resp.json())
+    .then(note => {
+      console.log(note)
+      this.props.deleteNote(note)
+      this.props.history.push('/dashboard')
+    })
+  }
+
   render() {
     return (
       <div className='render-note'>
         <h3>{this.props.note.title}</h3>
-        <p1>{this.props.note.content}</p1><br></br>
-        <button>Delete</button> <button>Edit</button>
+        <p>{this.props.note.content}</p><br></br>
+        <button onClick={() => this.handleOnClick(this.props.note)}>Delete</button> <button>Edit</button>
       </div>
     )
   }
